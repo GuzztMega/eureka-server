bd15151d655b4834aec52cf388ea1fde68d5c908
Augusto Longo <augusto@digitalsaude.com.br>
2023-06-22 21:45:02 -0300

    Started Eureka Server Project

---
+++ src/test/java/br/com/guzzmega/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guzzmega.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
