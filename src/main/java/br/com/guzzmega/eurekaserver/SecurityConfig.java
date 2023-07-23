4cc0736c7fa909bb15caa2145ebbea5789622946
Augusto Longo <guzzt.mega@gmail.com>
2023-07-23 16:24:49 -0300

    Added security configurations and swagger api documentation

---
+++ src/main/java/br/com/guzzmega/eurekaserver/SecurityConfig.java
@@ -0,0 +1,23 @@
+package br.com.guzzmega.eurekaserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf()
+				.disable()
+				.authorizeHttpRequests()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.httpBasic();
+
+		return httpSecurity.build();
+	}
+}
